a2f30b4d292831758430390a3e89445b92ae029e
Mattias Lasersköld <mattias.l.sk@gmail.com>
2022-12-14 19:40:00 +0100

    Create some kind of worknig profile

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 # involute-gears
 
 
+## References
+
 Implemented using instructions from this site:
 https://www.tec-science.com/mechanical-power-transmission/involute-gear/meshing-line-action-contact-pitch-circle-law/
+
+Other reference
+https://khkgears.net/new/gear_knowledge/abcs_of_gears-b/basic_gear_terminology_calculation.html
--- src/main.cpp
+++ src/main.cpp
@@ -14,7 +14,8 @@ using namespace glm;
 using FuncT = std::function<void(glm::vec2, glm::vec2)>;
 
 void drawLine(sdl::RendererView view, glm::vec2 p1, glm::vec2 p2) {
-    view.drawLine(p1.x, p1.y, p2.x, p2.y);
+    float scale = 1.5;
+    view.drawLine(p1.x * scale, p1.y * scale, p2.x * scale, p2.y * scale);
 }
 
 void drawArc(FuncT f,
@@ -31,6 +32,18 @@ void drawArc(FuncT f,
 
         f(p1, p2);
     }
+}
+
+void drawArc(sdl::RendererView view,
+             glm::vec2 center,
+             float r,
+             float start = 0,
+             float end = pi<float>() * 2) {
+    drawArc([view](auto p1, auto p2) { drawLine(view, p1, p2); },
+            center,
+            r,
+            start,
+            end);
 }
 
 struct Gear {
@@ -57,6 +70,14 @@ struct Gear {
         points.emplace_back(vec2{local});
     }
 
+    void rotatePoints(float angle) {
+        mat4 location = identity<mat4>();
+        location = rotate(location, angle, {0, 0, 1});
+        for (auto &p : points) {
+            p = location *vec4{p, 0, 1};
+        }
+    }
+
     void repeat(int num) {
         auto tmp = points;
         for (int i = 1; i < num; ++i) {
@@ -71,6 +92,21 @@ struct Gear {
         }
     }
 
+    void mirror() {
+        auto endSize = points.size();
+
+        auto newPoints = decltype(points){};
+        for (auto p : points) {
+            newPoints.push_back({p.x, -p.y});
+        }
+        reverse();
+        points.insert(points.begin(), newPoints.begin(), newPoints.end());
+    }
+
+    void reverse() {
+        std::reverse(points.begin(), points.end());
+    }
+
     glm::vec2 pos;
     float angle = 0;
 
@@ -78,9 +114,27 @@ struct Gear {
 };
 
 struct GearSettings {
+
+    GearSettings(float referenceDiameter,
+                 int numGears,
+                 float gearDepth = 20,
+                 float preassureAngle = 20.f / 180.f * pi<float>())
+        : referenceDiameter{referenceDiameter}
+        , gearDepth{gearDepth}
+        , preassureAngle{preassureAngle}
+        , numGears{numGears} {}
+
+    float referenceDiameter;
+    float gearDepth;
+    // Between 14 and 20 deg according to google
+    float preassureAngle;
+
+    float rootDiameter = referenceDiameter - gearDepth;
+    float tipDiameter = referenceDiameter + gearDepth;
+
     int numGears = 20;
-    float baseRadius = 100;
-    float addendumRadius = baseRadius + 10;
+    //    float baseRadius = 100;
+    //    float addendumRadius = baseRadius + 10;
 };
 
 int main(int argc, char **argv) {
@@ -100,39 +154,54 @@ int main(int argc, char **argv) {
 
     auto gear1 = Gear{};
     auto gear2 = Gear{};
+    auto gear3 = Gear{};
 
-    auto settings = GearSettings{};
+    auto settings = GearSettings{200, 20};
 
     gear1.pos = {200, 200};
-
-    auto overlap = 20;
-
-    gear2.pos = {gear1.pos.x + settings.baseRadius + settings.addendumRadius,
-                 200};
+    gear2.pos = {gear1.pos.x + settings.referenceDiameter, 200};
+    gear3.pos = {gear1.pos.x + settings.referenceDiameter, 200};
 
     auto center = (gear1.pos + gear2.pos) / 2.f;
 
     auto contactLength = 30.f;
 
     auto lineOfAction = [contactLength](float i) {
-        return glm::vec2{i / 2.f, i} * contactLength;
+        auto a = i;
+        return glm::vec2{(a - .5) * 1, (a - .5) * 1} * contactLength;
     };
 
     float contactPointIndex = 0;
 
     bool isRunning = true;
 
+    auto amountToAngle = pi<float>() * 2. / (settings.numGears) * 1.4;
+
+    //    gear1.points.push_back({settings.referenceDiameter / 2, 0});
+
     for (auto amount = 0.f; amount <= 1.; amount += 1. / 100.) {
-        auto angle = pi<float>() * 2 / settings.numGears * amount;
+        auto angle = amountToAngle * amount;
         gear1.angle = angle;
         gear2.angle = -angle;
+        gear3.angle = 0;
+        gear3.pos =
+            gear2.pos + vec2{0, angle * settings.referenceDiameter / 2.};
 
         gear1.addInverted(center + lineOfAction(amount));
         gear2.addInverted(center + lineOfAction(amount));
+        gear3.addInverted(center + lineOfAction(amount));
     }
 
+    renderer.scale(1, 1);
+
+    gear1.rotatePoints(pi<float>() * 2 / settings.numGears);
+    gear1.mirror();
     gear1.repeat(settings.numGears);
-    gear2.repeat(settings.numGears);
+    gear2.rotatePoints(-pi<float>() * 2 / settings.numGears);
+
+    //    gear1.mirror();
+    //    gear1.repeat(settings.numGears);
+    //    gear2.repeat(settings.numGears);
 
     for (; isRunning;) {
         for (auto event = std::optional<sdl::Event>{};
@@ -142,32 +211,35 @@ int main(int argc, char **argv) {
                 isRunning = false;
                 break;
             }
+            if (event->type == SDL_MOUSEMOTION) {
+                gear1.angle = 1. / 100. * event->motion.y - 1.;
+                gear2.angle = -gear1.angle;
+
+                gear3.pos =
+                    gear2.pos +
+                    vec2{0, gear1.angle * settings.referenceDiameter / 2.};
+
+                window.title(std::to_string(gear1.angle).c_str());
+            }
         }
         renderer.drawColor({100, 0, 0, 255});
         renderer.clear();
         renderer.drawColor({100, 100, 100, 255});
 
-        gear1.angle += .001;
-        gear2.angle -= .001;
+        //        gear1.angle += .001;
+        //        gear2.angle -= .001;
 
+        drawArc(renderer, gear1.pos, settings.referenceDiameter / 2);
+        drawArc(renderer, gear2.pos, settings.referenceDiameter / 2);
+        drawArc(renderer, gear1.pos, settings.rootDiameter / 2);
+        drawArc(renderer, gear2.pos, settings.rootDiameter / 2);
+        drawArc(renderer, gear1.pos, settings.tipDiameter / 2);
+        drawArc(renderer, gear2.pos, settings.tipDiameter / 2);
+
+        renderer.drawColor({200, 200, 200});
         gear1.draw(renderer);
         gear2.draw(renderer);
-
-        drawArc(
-            [&](auto p1, auto p2) {
-                drawLine(renderer, p1 + gear1.pos, p2 + gear1.pos);
-                drawLine(renderer, p1 + gear2.pos, p2 + gear2.pos);
-            },
-            {0, 0},
-            settings.baseRadius);
-
-        drawArc(
-            [&](auto p1, auto p2) {
-                drawLine(renderer, p1 + gear1.pos, p2 + gear1.pos);
-                drawLine(renderer, p1 + gear2.pos, p2 + gear2.pos);
-            },
-            {0, 0},
-            settings.addendumRadius);
+        gear3.draw(renderer);
 
         for (float i = 0; i < 1; i += .1) {
             auto p1 = lineOfAction(i) + center;
@@ -178,14 +250,17 @@ int main(int argc, char **argv) {
 
         renderer.drawColor({255, 255, 255});
 
-        contactPointIndex += .1;
-        if (contactPointIndex > 1) {
-            contactPointIndex = 0;
-        }
+        auto contactPoint = lineOfAction(gear1.angle / amountToAngle) + center;
+        contactPoint.x = center.x;
 
-        auto contactPoint = lineOfAction(contactPointIndex) + center;
+        drawLine(renderer,
+                 contactPoint + vec2{-100, 0},
+                 contactPoint + vec2{100, 0});
 
-        //        renderer.drawPoint(contactPoint);
+        drawLine(renderer,
+                 gear1.pos,
+                 gear1.createLocation() *
+                     vec4{settings.referenceDiameter / 2, 0, 0, 1});
 
         renderer.present();
         std::this_thread::sleep_for(10ms);
